1dec0ce6d20ded006f8cf3ce8a0ec7b869ed1d91
NorifumiKonishi <s.cute11sa@gmail.com>
2020-07-29 08:43:57 +0900

    修正点 全体的なインデントの修正 "=="から"==="へ もう一点、ご指摘頂いていた処理ですが、整数判定のために用いております。（is_intやis_numericでは正しく動作しなかったため）

--- input.php
+++ input.php
@@ -7,24 +7,23 @@
 </head>
 <body>
   <form action="input.php" method="post">
-    <p>FizzNum<input type="text" name="fizz"
-      placeholder="整数を入力してください"></p>
-    <p>BuzzNum<input type="text" name="buzz"
-      placeholder="整数を入力してください"></p>
+    <p>FizzNum<input type="text" name="fizz"placeholder="整数を入力してください"></p>
+    <p>BuzzNum<input type="text" name="buzz"placeholder="整数を入力してください"></p>
     <input type="submit"value="実行">
-      </form>
-    <h3>【出力】</h3>
-    <?php
+  </form>
+  <h3>【出力】</h3>
+<?php
 
-     if((empty($_POST['fizz'])) || (empty($_POST['buzz']))){//何も入力されてなければメッセージを表示
-     echo "未入力です。";
- }
- if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-     $num1 = $_POST["fizz"];
-     $num2 = $_POST["buzz"];
+// 最初にエラーを出さない為の処理
+  if((empty($_POST['fizz'])) || (empty($_POST['buzz']))){
+      echo "未入力です。";
+  }
+  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+       $num1 = $_POST["fizz"];
+       $num2 = $_POST["buzz"];
 
- if(strval($num1) === strval(intval($num1)) && (strval($num2)==strval(intval($num2))))
- {
+  if(strval($num1) === strval(intval($num1)) && (strval($num2)===strval(intval($num2)))){
+    // 整数判定
 
 
 
@@ -32,32 +31,30 @@
     $ans2 = 0;
     $i = 1;
     $j = 1;
-  while (($ans1 < 100) || ($ans2 < 100)){
-    $ans1 = $num1 * $i;
-    $ans2 = $num2 * $j;
-    if(($ans1 >= 100 ) || ($ans2 >= 100 )) {
-      break;}
+
+    while (($ans1 < 100) || ($ans2 < 100)){
+        $ans1 = $num1 * $i;
+        $ans2 = $num2 * $j;
+      if(($ans1 >= 100 ) || ($ans2 >= 100 )) {
+        break;}
 
 
-      if($ans1 == $ans2) {
+      if($ans1 === $ans2) {
         echo "fizzBuzz".$ans1."<br>";
-        $i++;
-        $j++;
+          $i++;
+          $j++;
       }elseif($ans1 < $ans2) {
         echo "fizz".$ans1."<br>";
-        $i++;
+          $i++;
       }else{
         echo "Buzz".$ans2."<br>";
-        $j++;
+          $j++;
       }
-
-    }
-
- }else{
-   echo "整数を入力してください";
- }
+    }//while終了
+  }else{
+    echo "整数を入力してください";
+  }
 }
-
 ?>
 </body>
 </html>
